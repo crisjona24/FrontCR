dbfd4573d8391b85e528dc1137b8fa5c62b59cf1
Cristobal Jonathan Rios Simancas <78310047+crisjona24@users.noreply.github.com>
2023-12-28 19:55:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cors from "cors";
+
+export default {
+  ...,
+  plugins: [
+    cors({
+      origin: "*",
+    }),
+  ],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
